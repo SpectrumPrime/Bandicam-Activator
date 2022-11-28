8ed8a3f47848c1e41abed63bc871992547140d41
SpectrumPrime <55854936+SpectrumPrime@users.noreply.github.com>
2022-11-28 22:56:30 +0200

    READ TXT BREFORE RUNNING BAT

--- Connectivity fix/Offline Bandicam.bat
+++ Connectivity fix/Offline Bandicam.bat
@@ -3,20 +3,15 @@ if not "%1"=="am_admin" (
     powershell -Command "Start-Process -Verb RunAs -FilePath '%0' -ArgumentList 'am_admin'"
     exit /b
 )
-
 @echo off
-
 :: Variables
 set RULE_NAME=TemporaryBlock
 set PROGRAM=D:\Bandicam\bdcam.exe
-
 :: Blocking connectivity
 netsh advfirewall firewall set rule name="%RULE_NAME%" dir=in new enable=yes
 netsh advfirewall firewall set rule name="%RULE_NAME%" dir=out new enable=yes
-
 :: Running the app
 start "" /wait "%PROGRAM%"
-
 :: Disabling firewall rules
 netsh advfirewall firewall set rule name="%RULE_NAME%" dir=in new enable=no
 netsh advfirewall firewall set rule name="%RULE_NAME%" dir=out new enable=no
